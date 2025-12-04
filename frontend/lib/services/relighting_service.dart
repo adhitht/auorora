88c361993010cdac0f66e68ad32a0b92733d20dd
adhitht <hello@adhitht.me>
2025-12-04 21:04:08 +0530

    feat: processing in the cloud notification for gRPC calls

--- frontend/lib/services/relighting_service.dart
+++ frontend/lib/services/relighting_service.dart
@@ -5,6 +5,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:grpc/grpc.dart';
 import 'package:aurora/models/relighting_model.dart';
 import 'package:aurora/generated/relighting.pbgrpc.dart';
+import '../service_locator.dart';
+import 'notification_service.dart';
 
 class RelightingService {
   late RelightingServiceClient _stub;
@@ -41,6 +43,9 @@ class RelightingService {
       if (maskBytes != null) {
         request.maskData = maskBytes;
       }
+
+      final notificationService = getIt<NotificationService>();
+      notificationService.show("Processing in the cloud", type: NotificationType.info);
 
       final response = await _stub.relight(request);
       return Uint8List.fromList(response.processedImageData);
