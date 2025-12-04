538f0fa024b0ad24bddb06d1f612c0ba29309ed3
harsh15044 <75984501+harsh15044@users.noreply.github.com>
2025-12-04 21:41:33 +0530

    feat: remove standalone download script for Neural Gaffer checkpoints; integrate download into init.sh

--- init.sh
+++ init.sh
@@ -23,32 +23,11 @@ echo "Success! Files generated:"
 
 
 echo "Downloading Neural Gaffer checkpoints"
-
-#details of the submission repo
-OWNER="adhitht"
-REPO="adobe"
-TAG="ipynbs"
-ASSET_NAME="Copy_of_Relight_pipe.1.ipynb"
-TOKEN="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"
-
-# 2. Fetch release JSON to find asset ID
-ASSET_ID=$(curl -s \
-  -H "Authorization: Bearer ${TOKEN}" \
-  -H "Accept: application/vnd.github+json" \
-  "https://api.github.com/repos/${OWNER}/${REPO}/releases/tags/${TAG}" \
-  | jq -r ".assets[] | select(.name==\"${ASSET_NAME}\") | .id")
-
-
-# 3. go to the correct directory
+# 1. go to the correct directory
 cd backend/ml_models/relighting
 
-
-# 4. Download the asset using its ID
-curl -L \
-  -H "Authorization: Bearer ${TOKEN}" \
-  -H "Accept: application/octet-stream" \
-  "https://api.github.com/repos/${OWNER}/${REPO}/releases/assets/${ASSET_ID}" \
-  -o "${ASSET_NAME}"
+# 2. Download the 
+wget -q https://huggingface.co/coast01/Neural_Gaffer/resolve/main/neural_gaffer_res256_ckpt.zip -O neural_gaffer_res256_ckpt.zip
 
 # 5. Unzip the model weights
 unzip -q neural_gaffer_res256_ckpt.zip
